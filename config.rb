d8bc1724d9bb8c74e2e72a46bcb20f0d59d24c7b
dishuiGit <dishui_git@126.com>
2016-03-21 00:41:14 +0800

    2016-03-21

--- config.rb
+++ config.rb
@@ -4,15 +4,3 @@ GitHub::Markup.markup(:asciidoctor, /adoc|asc(iidoc)?/) do |content|
   Asciidoctor::Compliance.unique_id_start_index = 1
   Asciidoctor.convert(content, :safe => :safe, :attributes => %w(showtitle=@ idprefix idseparator=- env=github env-github source-highlighter=html-pipeline))
 end
-
-# always include this:
-Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
-
-# remove the original AsciiDoc binding:
-Gollum::Markup.formats.delete(:asciidoc)
-
-# and define your own (".asc" is the new primary extension):
-Gollum::Markup.formats[:adoc] = {
-    :name => "AsciiDoc",
-    :regexp => /adoc|asc(iidoc)?/
-}
--- tmp.md
+++ tmp.md
@@ -7,3 +7,17 @@
 ```bash
 yum localinstall VirtualBox-4.3-4.3.36_105129_el5-1.i386.rpm --nogpgcheck
 ```
+
+
+
+# always include this:
+Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
+
+# remove the original AsciiDoc binding:
+Gollum::Markup.formats.delete(:asciidoc)
+
+# and define your own (".asc" is the new primary extension):
+Gollum::Markup.formats[:adoc] = {
+    :name => "AsciiDoc",
+    :regexp => /adoc|asc(iidoc)?/
+}
