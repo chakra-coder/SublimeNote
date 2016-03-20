33423c20adee4b8b43e725362ebc69ec86054050
dishuiGit <dishui_git@126.com>
2016-03-20 21:32:05 +0800

    2016-03-20

--- config.rb
+++ config.rb
@@ -1,5 +1,18 @@
+GitHub::Markup.markups.reject! {|markup| markup.regexp.to_s == '(?-mix:adoc|asc(iidoc)?)' }
+
+GitHub::Markup.markup(:asciidoctor, /adoc|asc(iidoc)?/) do |content|
+  Asciidoctor::Compliance.unique_id_start_index = 1
+  Asciidoctor.convert(content, :safe => :safe, :attributes => %w(showtitle=@ idprefix idseparator=- env=github env-github source-highlighter=html-pipeline))
+end
+
 # always include this:
 Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
 
-# bind your own extension regex (the new set of extensions will also include `.asc` and `.adoc`):
-Gollum::Markup.formats[:asciidoc][:regexp] = /(?:asciidoc|asc|adoc)/
+# remove the original AsciiDoc binding:
+Gollum::Markup.formats.delete(:asciidoc)
+
+# and define your own (".asc" is the new primary extension):
+Gollum::Markup.formats[:adoc] = {
+    :name => "AsciiDoc",
+    :regexp => /adoc|asc(iidoc)?/
+}
