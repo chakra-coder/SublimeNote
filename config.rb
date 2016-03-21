1fd590bf92660365893e5f47f2719b93c21f75cf
dishuiGit <dishui_git@126.com>
2016-03-21 17:43:20 +0800

    2016-03-21

--- config.rb
+++ config.rb
@@ -4,3 +4,15 @@ GitHub::Markup.markup(:asciidoctor, /adoc|asc(iidoc)?/) do |content|
   Asciidoctor::Compliance.unique_id_start_index = 1
   Asciidoctor.convert(content, :safe => :safe, :attributes => %w(showtitle=@ idprefix idseparator=- env=github env-github source-highlighter=html-pipeline))
 end
+
+# always include this:
+Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
+
+# remove the original AsciiDoc binding:
+Gollum::Markup.formats.delete(:asciidoc)
+
+# and define your own (".asc" is the new primary extension):
+Gollum::Markup.formats[:asc] = {
+    :name => "AsciiDoc",
+    :regexp => /adoc|asc(iidoc)?/
+}
