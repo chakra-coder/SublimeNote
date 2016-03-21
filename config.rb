59b702562bd3c48b957f2b0dad2f8033d4ed6d06
dishuiGit <dishui_git@126.com>
2016-03-21 18:10:11 +0800

    2016-03-21

--- config.rb
+++ config.rb
@@ -1,10 +1,3 @@
-GitHub::Markup.markups.reject! {|markup| markup.regexp.to_s == '(?-mix:adoc|asc(iidoc)?)' }
-
-GitHub::Markup.markup(:asciidoctor, /adoc|asc(iidoc)?/) do |content|
-  Asciidoctor::Compliance.unique_id_start_index = 1
-  Asciidoctor.convert(content, :safe => :safe, :attributes => %w(showtitle=@ idprefix idseparator=- env=github env-github source-highlighter=html-pipeline))
-end
-
 # always include this:
 Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
 
@@ -16,3 +9,10 @@ Gollum::Markup.formats[:asc] = {
     :name => "AsciiDoc",
     :regexp => /adoc|asc(iidoc)?/
 }
+
+GitHub::Markup.markups.reject! {|markup| markup.regexp.to_s == '(?-mix:adoc|asc(iidoc)?)' }
+
+GitHub::Markup.markup(:asciidoctor, /adoc|asc(iidoc)?/) do |content|
+  Asciidoctor::Compliance.unique_id_start_index = 1
+  Asciidoctor.convert(content, :safe => :safe, :attributes => %w(showtitle=@ idprefix idseparator=- env=github env-github source-highlighter=html-pipeline))
+end
